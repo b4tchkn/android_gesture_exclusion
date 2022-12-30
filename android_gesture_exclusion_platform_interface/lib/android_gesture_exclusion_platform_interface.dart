f88cd6984142ea0b203b19d95c86236504583e3a
b4tchkn <baaaakkrad@gmail.com>
2022-12-30 17:05:18 +0900

    chore: add comments on PlatformInterface

--- android_gesture_exclusion_platform_interface/lib/android_gesture_exclusion_platform_interface.dart
+++ android_gesture_exclusion_platform_interface/lib/android_gesture_exclusion_platform_interface.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 import 'package:android_gesture_exclusion_platform_interface/method_channel_android_gesture_exclusion.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
+/// The interface each platform implementation must implement.
 abstract class AndroidGestureExclusionPlatform extends PlatformInterface {
+  /// Constructs an AndroidGestureExclusionPlatform.
   AndroidGestureExclusionPlatform() : super(token: _token);
 
   static final Object _token = Object();
@@ -11,8 +13,13 @@ abstract class AndroidGestureExclusionPlatform extends PlatformInterface {
   static AndroidGestureExclusionPlatform _instance =
       MethodChannelAndroidGestureExclusionPlatformInterface();
 
+  /// The default instance of [AndroidGestureExclusionPlatform] to use.
+  ///
+  /// Defaults to [MethodChannelAndroidGestureExclusionPlatformInterface].
   static AndroidGestureExclusionPlatform get instance => _instance;
 
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [AndroidGestureExclusionPlatform] when they register themselves.
   static set instance(AndroidGestureExclusionPlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
